937757b3b1842cdba7e412249a838a8f6b8f0c00
Maxwell Paul Brickner <mbrickn@users.noreply.github.com>
2018-01-24 15:21:24 -0500

    Create butter.js
    Created a basic component file.

---
+++ butter.js
@@ -0,0 +1,12 @@
+AFRAME.registercomponent('butter', {
+schema: {
+  color: {default: '#F4B9B3'},
+  flatshading: {default: true}
+},
+init: function () {},
+update: function () {},
+tick: function () {},
+remove: function () {},
+pause: function () {},
+play: function () {}
+});
